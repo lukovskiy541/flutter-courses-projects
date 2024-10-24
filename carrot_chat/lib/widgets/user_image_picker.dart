a2e6da3663ce352359d8ce6c7e3759060d640bf3
Max Lukovskiy <ragekrimson@gmail.com>
2024-10-24 23:15:38 +0300

    carrot-chat

---
+++ carrot_chat/lib/widgets/user_image_picker.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class UserImagePicker extends StatefulWidget {
+  const UserImagePicker({super.key});
+
+  @override
+  State<UserImagePicker> createState() => _UserImagePickerState();
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(),
+        TextButton.icon(onPressed: onPressed, label: label)
+      ],
+    );
+  }
+}
